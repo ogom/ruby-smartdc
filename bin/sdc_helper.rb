d4e4091517bd325ff00e248c716ba60c974f1733
ogom <ogom@hotmail.co.jp>
2011-11-06 20:54:56 +0900

    fix helper

--- bin/sdc_helper.rb
+++ bin/sdc_helper.rb
@@ -1,9 +1,11 @@
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', '..', 'lib'))
+#!/usr/bin/env ruby
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'pp'
 require 'smartdc'
 
 def fixture(name)
-  json = File.read(File.join(File.dirname(__FILE__), '..', "fixtures/#{name}.json"))
+  json = File.read(File.join(File.dirname(__FILE__), "fixtures/#{name}.json"))
   MultiJson.decode(json)
 end
 
