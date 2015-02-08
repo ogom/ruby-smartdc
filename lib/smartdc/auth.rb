8898df4e5f886e603298b81592fdeda9fda5d00d
ogom <ogom@hotmail.co.jp>
2015-02-08 20:59:41 +0900

    Added fingerprint to auth

--- lib/smartdc/auth.rb
+++ lib/smartdc/auth.rb
@@ -1,12 +1,50 @@
 require 'openssl'
 
 module Smartdc
-  module Auth
-    def self.sign(request={}, options={})
-      rsa = OpenSSL::PKey::RSA.new File.read options[:rsa_path]
+  class Auth
+    attr_reader :username, :rsa_path, :use_key
+
+    def initialize(options={})
+      @username = options[:username]
+      @rsa_path = options[:rsa_path] ||= '~/.ssh'
+      @use_key = options[:use_key]
+    end
+
+    def find_key_file
+      path = File.expand_path(self.rsa_path)
+      if File.directory?(path)
+        Dir[File.join(path, '*')].each do |path|
+          if File.file?(path)
+            if self.use_key == self.fingerprint(path)
+              return path
+            end
+          end
+        end
+        return nil
+      else
+        return path
+      end
+    end
+
+    def read_key_file(path)
+      OpenSSL::PKey::RSA.new(File.read(File.expand_path(path)), '')
+    end
+
+    def signature(data)
+      rsa = read_key_file(self.find_key_file)
       sha256 = OpenSSL::Digest::SHA256.new
-      raw = [rsa.sign(sha256, request[:headers][:date])].pack('m').delete("\r\n")
-      "Signature keyId=\"/#{options[:username]}/keys/#{options[:use_key]}\",algorithm=\"rsa-sha256\" #{raw}"
+      str = [rsa.sign(sha256, data)].pack('m').delete("\r\n")
+      "Signature keyId=\"/#{self.username}/keys/#{self.use_key}\",algorithm=\"rsa-sha256\" #{str}"
+    rescue
+      nil
+    end
+
+    def fingerprint(path)
+      rsa = read_key_file(path)
+      str = [7].pack('N') + 'ssh-rsa' + rsa.public_key.e.to_s(0) + rsa.public_key.n.to_s(0)
+      OpenSSL::Digest::MD5.hexdigest(str).scan(/../).join(':')
+    rescue
+      nil
     end
   end
 end
